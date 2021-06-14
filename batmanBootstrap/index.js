71d604fd381eb5b2156912fa0105f22fd0bfcac1
philliprose1 <77714582+philliprose1@users.noreply.github.com>
2021-06-13 21:45:13 -0700

    Add files via upload

---
+++ batmanBootstrap/index.js
@@ -0,0 +1,9 @@
+function batman(){
+    alert('I am Batman!')
+}
+function joker(){
+    alert('WHY SO SERIOUS!')
+}
+function harley(){
+alert('I`m Rubber, You`re Glue, Whatever You Say Bounces Off Me And Makes A Six-Inch-Diameter Exit Wound In You')
+}
